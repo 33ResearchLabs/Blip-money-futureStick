2b1148989a033925f200bf28e50a52ee9a145abc
33ResearchLabs <33researchlabs@gmail.com>
2025-12-10 12:03:40 +0530

    added footer

--- blip-protocol-ui/src/pages/Index.tsx
+++ blip-protocol-ui/src/pages/Index.tsx
@@ -1521,6 +1521,16 @@ Payments"
           </div>
         </div>
       </section>
+
+      {/* <!-- Footer --> */}
+      <footer className="py-12 border-t border-gray-900 mt-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-500 text-sm">
+          <p>
+            &copy; <span id="year">2025</span> Blip.money Protocol. All Rights
+            Reserved. Bankless. Trustless. Instant.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
