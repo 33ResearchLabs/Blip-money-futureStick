376d6739d0c7db59f6ccf3640ce3a635fca4d0af
33ResearchLabs <33researchlabs@gmail.com>
2026-02-12 13:12:52 +0530

    test

--- blip-protocol-ui-test/src/App.tsx
+++ blip-protocol-ui-test/src/App.tsx
@@ -105,7 +105,7 @@ const App = () => (
         >
           <TooltipProvider>
             <Toaster />
-            {/* <Sonner /> */}
+            <Sonner />
 
             <BrowserRouter>
               <GoogleAnalytics />
